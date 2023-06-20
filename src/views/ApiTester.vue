227bbca731cb00ce0630564b30202417e9e44e58
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2023-06-20 14:08:29 -0300

    finalizando implementação de teste de comunicação

--- src/views/ApiTester.vue
+++ src/views/ApiTester.vue
@@ -3,20 +3,42 @@
         <v-row no-gutters>
             <v-col cols="12"> 
                 <v-card
-                    :loading="false"
+                    :loading="loading"
                     title="Teste de API"
                     variant="tonal"
-                    subtitle="Teste para verificar se a comunicação entre o cliente e API está correta"
+                    subtitle="Verifique se a comunicação entre o cliente e a API está correta"
                 >
                     <v-card-text>
                         <v-col cols="12" class="mx-0 px-0" v-if="consultaExecutada">
                             <div class="font-weight-bold mx-0 px-0">
-                                Resultado:
+                                Resultado: 
+                                <v-chip
+                                  class="ma-2"
+                                  color="green"
+                                  text-color="white"
+                                  v-if="response"
+                                >
+                                  Comunicação realizada
+                                </v-chip>
+                                <v-chip
+                                  class="ma-2"
+                                  color="red"
+                                  text-color="white"
+                                  v-if="error"
+                                >
+                                  Falha na comunicação
+                                </v-chip>
                             </div>
                         </v-col>
                     </v-card-text>
                     <v-card-actions>
-                        <v-btn color="green-darken-1" variant="tonal" @click="consultar()">Executar</v-btn>
+                        <v-btn 
+                          color="green-darken-1" 
+                          variant="tonal" 
+                          @click="consultar()"
+                          :disabled="loading"
+                          :loading="loading"
+                        >Executar</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-col>
@@ -29,10 +51,6 @@ import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'
 import axios from 'axios'
 
-interface Todo {
-  text: string;
-}
-
 const App = defineComponent({
   components: {
     HelloWorld
@@ -41,13 +59,39 @@ const App = defineComponent({
   data() {
     return {
       response: false,
+      error: false,
       consultaExecutada: false,
-      serverUrl: import.meta.env.VITE_SERVER_URL
+      serverUrl: import.meta.env.VITE_SERVER_URL,
+      loading: false
     };
   },
   methods: {
-    consultar(){
-        alert(this.serverUrl)
+    async consultar(){
+      let url = this.serverUrl+'api/test'
+      this.loading = true
+      await axios.get(url)
+      .then((response) => {
+        this.resetMessages()
+        this.consultaExecutada = true
+        if(response.data.response === true){
+          this.response = true
+        }else{
+          this.response = false
+        }
+      })
+      .catch(error => {
+        this.resetMessages()
+        this.consultaExecutada = true
+        this.error = true
+        console.error(error);
+      });
+    },
+
+    resetMessages(){
+      this.response = false
+      this.error = false
+      this.consultaExecutada = false
+      this.loading = false
     }
   },
 
